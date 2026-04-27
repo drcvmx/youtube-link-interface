a72586b54eeff2dc1c0bb8907440f86eabe827a6
drcvmx <drcv.work.code@gmail.com>
2026-04-26 19:27:59 -0600

    fix: page.tsx corregido desde servidor

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -770,7 +770,6 @@ export default function YouTubeLinkSubmission() {
           </div>
         </div>
       </div>
-    </div>
   )
 }
 
@@ -781,7 +780,7 @@ function TargetIcon(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 24"
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
@@ -794,3 +793,4 @@ function TargetIcon(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
